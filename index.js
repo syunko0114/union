176945720eb85386eb6624cb306919fbd89b9508
syunko0114 <92129382+syunko0114@users.noreply.github.com>
2022-05-06 11:35:11 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -59,12 +59,20 @@ $(window).scroll(function(){
   }else{
   }
 })
-$(window).scroll(function(){
-  var top = $("#service04").offset().top; // ターゲットの位置取得
-  var position = top - $(window).height();  // ターゲットが上からスクロールしたときに見える位置
-  var position_bottom = top + $("#service04").height();  // ターゲットが下からスクロールしたときに見える位置
-  if($(window).scrollTop() > position && $(window).scrollTop() < position_bottom){
-      $("#service04").addClass("active");
-  }else{
+
+var windowHeight = document.documentElement.clientHeight;
+
+$(window).scroll(function () {
+  if($(window).scrollTop() > windowHeight) {
+    $('#nav').addClass('color_change');
+  } else {
+    $('#nav').removeClass('color_change');
   }
-})
+});
+$(window).scroll(function () {
+  if($(window).scrollTop() > windowHeight/2) {
+    $('#header_fixed_text').addClass('color_change');
+  } else {
+    $('#header_fixed_text').removeClass('color_change');
+  }
+});
